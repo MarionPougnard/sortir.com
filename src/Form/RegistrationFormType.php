a6c5e8a011da689383ad4827c74539bc3097db69
Marion <marion.pougnard@protonmail.com>
2024-09-26 14:32:37 +0200

    rebase

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -7,7 +7,6 @@ use App\Entity\Utilisateur;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
