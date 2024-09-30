f8c72fa6609666cbbd1ff926ea45859263f4a138
Sandie79 <guerin-sandie@hotmail.fr>
2024-09-30 10:13:17 +0200

    feat : gerer ville 2

---
+++ src/Form/VilleType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Ville;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class VilleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+
+        $builder
+            ->add('nom', TextType::class, ['label' => 'Ville', 'attr' => ['class' => 'form-control']])
+            ->add('codePostal', TextType::class, ['label' => 'Code postal', 'attr' => ['class' => 'form-control']])
+            ->add('save', SubmitType::class, ['label' => 'Enregistrer', 'attr' => ['class' => 'btn btn-success']]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Ville::class,
+        ]);
+    }
+}
